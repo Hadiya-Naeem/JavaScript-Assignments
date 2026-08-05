e3093a5ffa8ef968633e925c5afbb994704105a1
Hadiya-Naeem <nhadiya415@gmail.com>
2026-08-06 02:01:21 +0500

    chapter 12-13

---
+++ Chapter12-13/aap.js
@@ -0,0 +1,63 @@
+// Question 1
+
+
+// Question 2
+// var num1 = +prompt("write first integar")
+// var num2 = +prompt("write secon integar")
+
+// if(num1 > num2){
+//     document.write("The larger number is:" + num1);
+// }
+// else if(num1< num2){
+//      document.write("The larger number is:" + num2);
+// }
+// else{
+//      document.write("Both integers are same");
+// }
+
+// Question 3
+// var num =+prompt("Enter a number")
+// if(num > 0){
+//    document.write(num + " is a Positive number");
+// }
+// else if(num< 0){
+//     document.write(num + " is a Negative number.");
+// }
+// else{
+//     document.write("The number is 0");
+// }
+
+// Question 4
+
+
+
+
+// Question 5
+// var correctPassword = pass123;
+// var userPass =  prompt("Enter your password");
+
+// if(userPass = null){
+//     alert("Please enter your password");
+// }
+// else if(userPass = correctPassword){
+//     alert("“Correct! The password you entered matches the original password");
+// }
+// else{
+//     alert("Invalid Password");
+// }
+
+// Question 6
+
+// var greeting;
+// var hour = 13;
+
+// if (hour < 18) {
+//     greeting = "Good day";
+// }
+// else {
+//     greeting = "Good evening";
+// }
+
+// document.write(greeting);
+
+// Question 7
