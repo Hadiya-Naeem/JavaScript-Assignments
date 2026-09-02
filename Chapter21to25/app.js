c21f0544d45bf991e74652ee4beac2beddc9b32d
Hadiya-Naeem <nhadia415@gmail.com>
2026-09-03 03:04:06 +0500

    chp21to25 till qustion 11

---
+++ Chapter21to25/app.js
@@ -0,0 +1,94 @@
+// Question 1
+// var firstName = prompt("Enter yoour first name")
+// var lastName = prompt("Enter yoour last name")
+// var fullName = firstName + lastName
+
+// alert("Hi! " + firstName + " " + lastName)
+
+// Question 2
+
+// var favMobile = prompt("What is your fav mobile phone model")
+
+// document.write("My favourite mobile is: " + favMobile)
+// document.write("<br/>")
+// document.write("Length of string: " + favMobile.length)      
+
+// Question 3
+
+// var word = "pakistani"
+// var indexOfn = word.indexOf("n")
+// document.write("string: " + word + "<br/>")
+
+// document.write("index of n: " + indexOfn)
+
+// Question 4
+
+// var word = "Hello World"
+// var index = word.lastIndexOf("l")
+
+// document.write("String: " + word + "<br/>")
+// document.write("Last index of l  is: " + index)
+
+// Question 5
+// var word = "Pakistani"
+// var index =  word.charAt(3)
+// document.write("String: " + word + "<br/>")
+// document.write("letter at 3rd index: " + index)
+
+// Question 6
+// var firstName = prompt("Enter yoour first name")
+// var lastName = prompt("Enter yoour last name")
+
+// var fullName = firstName.concat(" " + lastName)
+// alert("Hi! " + fullName)
+
+// Question 7
+
+// var city = "hyderabad"
+// var replaced = city.replace("hyder" , "islam")
+
+// document.write("City: " + city + "<br/>")
+// document.write("Replaced: " + replaced)
+
+// Question 8
+// var para = "Ali and Sami are best friend and the play cricket and football together"
+
+// var replaced = para.replaceAll("and" , "&")
+// document.write(para + "<br/>")
+// document.write(replaced)
+
+// Question 9
+// var num = "472"
+
+// var result = parseInt(num)
+// document.write("Value: " + num + "<br/>")
+// document.write("Type: " + typeof(num) + "<br/>")
+// document.write("Value: " + result + "<br/>")
+// document.write("Type: " + typeof(result))
+
+// Question 10
+// var userName =prompt("Enter your name")
+// var uppercase = userName.toUpperCase()
+// document.write("User input: " + userName + "<br/>")
+// document.write("Uppercase: " + uppercase)
+
+// Question 11
+// var input = prompt("Enter a word:");
+
+// var result = input[0].toUpperCase() + input.slice(1);
+
+// document.write("User input: " + input + "<br>");
+// document.write("Title case: " + result);
+
+// Question 12
+
+// var num = 35.36;
+
+// var str = num.toString();
+
+// var result = str.replace(".", "");
+
+// document.write("Number: " + num + "<br>");
+// document.write("Result: " + result);
+
+// Question 13
