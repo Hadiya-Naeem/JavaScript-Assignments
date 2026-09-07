be94da7ef6320870a318bf2f9ef313a206929afe
Hadiya-Naeem <nhadia415@gmail.com>
2026-09-08 02:28:18 +0500

    chp 26 to 30

---
+++ Chapter26to30/app.js
@@ -0,0 +1,67 @@
+// Question 1
+
+// var number = Number(prompt("Enter a positive number"))
+// document.write("Number: " + number + "</br>")
+// document.write("Roundoff value: " + Math.round(number) + "</br>")
+// document.write("floor value: " + Math.floor(number) + "</br>")
+// document.write("ceil value: " + Math.ceil(number) + "</br>")
+
+// Question 2
+
+// var number = Number(prompt("Enter a negative number"))
+// document.write("Number: " + number + "</br>")
+// document.write("Roundoff value: " + Math.round(number) + "</br>")
+// document.write("floor value: " + Math.floor(number) + "</br>")
+// document.write("ceil value: " + Math.ceil(number) + "</br>")
+
+// Question 3
+
+// var number = Number(prompt("Enter a number"))
+// document.write("Absolute value of: " + number + " is " + Math.abs(number))
+
+// Question 4
+
+// var dice = Math.floor(Math.random() * 6) + 1;
+
+// document.write("Dice value: " + dice);
+
+// Question 5
+// var player1Name = prompt("first player's your name")
+// var player1 = prompt(player1Name + " what will you choose head or tail")
+// var player2Name = prompt("second player's your name")
+// var player2 = prompt(player2Name + " what you will choose head or tail")
+// var random = Math.random()
+// if(random <= 0.5){
+//     alert(player1Name + " you win")
+// }
+// else{
+//     alert(player2Name + " you win")
+// }
+
+
+// Question 6
+
+// var random =Math.floor( Math.random() *100) + 1;
+
+// document.write("random number between 1 to 100: " + random)
+
+// Question 7
+
+// var weight = prompt("Enter your weight:");
+
+// var parsedWeight = parseFloat(weight);
+
+// document.write("Your weight is: " + parsedWeight + " kg");
+
+// Question 8 
+
+// var secretNumber = Math.floor(Math.random() * 10) + 1;
+
+// var userNumber = prompt("Enter a number between 1 and 10:");
+
+// if (userNumber == secretNumber) {
+//     document.write("Congratulations! You guessed the secret number.");
+// }
+// else{
+//     alert("try again")
+// }
