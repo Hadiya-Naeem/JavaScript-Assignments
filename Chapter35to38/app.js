15f826676c279c500260a0e14636f3935023c7d7
Hadiya-Naeem <nhadiya415@gmail.com>
2026-09-14 00:38:32 +0500

    9 queations done

---
+++ Chapter35to38/app.js
@@ -0,0 +1,147 @@
+// Question 1
+
+// let date = new Date();
+// document.write(date);
+
+// Question 2
+
+// let firstName = prompt("Enter your first name")
+// let lastName = prompt("Enter your last name")
+
+// function greet(name1, name2){
+
+// document.write(`hi! ${name1} ${name2}`)
+
+// }
+
+// greet(firstName, lastName )
+// Question 3
+
+// let num1 = Number(prompt("Enter first number for addition"))
+// let num2 = Number(prompt("Enter second number for addition"))
+
+// function add(number1 , number2){
+// document.write(`add: ${number1 + number2} `)
+// }
+// add(num1 ,num2)
+
+// Question 4
+
+// let num1 = Number(prompt("Enter first number"));
+// let num2 = Number(prompt("Enter second number"));
+// let operator = prompt("Enter the opertaor: (+,-,/,*,%)")
+
+// function operate(firstNum,secondNum,sign){
+//  if(sign === "+"){
+//     document.write(`addition: ${firstNum +secondNum}`)
+//  }
+//  else if(sign === "-"){
+//     document.write(`subtraction: ${firstNum - secondNum}`)
+//  }
+//   else if(sign === "*"){
+//     document.write(`multiplication: ${firstNum * secondNum}`)
+//  }
+//   else if(sign === "/"){
+//     document.write(`sub: ${firstNum / secondNum}`)
+//  }
+//   else if(sign === "%"){
+//     document.write(`sub: ${firstNum % secondNum}`)
+//  }
+
+// }
+// operate(num1,num2,operator)
+
+// Question 5
+
+// let num = Number(prompt("Enter a number"));
+
+// function square(number){
+//   return number*number
+
+// }
+
+// let result = square(num)
+
+// document.write(result)
+
+
+// Question 6
+
+// function factorial(num) {
+//     let result = 1;
+
+//     for (i = num; i >= 1; i--) {
+//         result = result * i;
+
+//     }
+//      return result
+// }
+
+// let answer = factorial(Number(prompt("Enter a number")))
+// document.write(answer)
+
+// Question 7
+
+// function counting(num1, num2){
+// for(i = num1; i <= num2; i++){
+// document.write(`${i} <br />`)
+// }
+// }
+// let firstNumber = Number(prompt("Enter first number"));
+// let secondNumber = Number(prompt("Enter second number"));
+
+// counting(firstNumber,secondNumber)
+
+// Question 8
+
+// function calculateHypotenuse(base, perpendicular) {
+
+//     function calculateSquare(number) {
+//         return number * number;
+//     }
+
+//     var baseSquare = calculateSquare(base);
+//     var perpendicularSquare = calculateSquare(perpendicular);
+
+//     var hypotenuse = Math.sqrt(baseSquare + perpendicularSquare);
+
+//     return hypotenuse;
+// }
+
+
+// var base = prompt("Enter the base:");
+// var perpendicular = prompt("Enter the perpendicular:");
+
+// var result = calculateHypotenuse(Number(base), Number(perpendicular));
+
+// document.write("Hypotenuse = " + result);
+
+
+// Question 9
+
+// function calculateArea(width, height) {
+//     return width * height;
+// }
+
+// var area = calculateArea(10, 5);
+
+// document.write("Area of rectangle = " + area);
+
+
+// function calculateArea(width, height) {
+//     return width * height;
+// }
+
+// var width = 10;
+// var height = 5;
+
+// var area = calculateArea(width, height);
+
+// document.write("Area of rectangle = " + area);
+
+
+// Question 10
+
+
+
+
